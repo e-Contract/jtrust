2a18097f19bfcc26fcc589a2947fa9191e81ad55
frank.cornelis <frank.cornelis@2407b8e0-faca-11dd-a8e2-f7cb3c35fcff>
2009-05-04 14:32:30 +0000

    improved the CRL cache

--- trunk/src/main/java/be/fedict/trust/CachedCrlRepository.java
+++ trunk/src/main/java/be/fedict/trust/CachedCrlRepository.java
@@ -18,6 +18,7 @@
 
 package be.fedict.trust;
 
+import java.lang.ref.SoftReference;
 import java.net.URI;
 import java.security.cert.X509CRL;
 import java.util.Date;
@@ -26,10 +27,11 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
 
 /**
  * A cached CRL repository implementation. This CRL repository will cache CRLs
- * in memory in case the nextUpdate is not yet reached.
+ * in memory.
  * 
  * @author fcorneli
  * 
@@ -38,9 +40,13 @@ public class CachedCrlRepository implements CrlRepository {
 
 	private static final Log LOG = LogFactory.getLog(CachedCrlRepository.class);
 
-	private final Map<URI, X509CRL> crlCache;
+	public static final int DEFAULT_CACHE_AGING_HOURS = 3;
+
+	private final Map<URI, SoftReference<X509CRL>> crlCache;
 
 	private final CrlRepository crlRepository;
+
+	private int cacheAgingHours;
 
 	/**
 	 * Main constructor.
@@ -50,15 +56,38 @@ public class CachedCrlRepository implements CrlRepository {
 	 */
 	public CachedCrlRepository(CrlRepository crlRepository) {
 		this.crlRepository = crlRepository;
-		this.crlCache = new HashMap<URI, X509CRL>();
+		this.crlCache = new HashMap<URI, SoftReference<X509CRL>>();
+		this.cacheAgingHours = DEFAULT_CACHE_AGING_HOURS;
 	}
 
 	public X509CRL findCrl(URI crlUri, Date validationDate) {
-		X509CRL crl = this.crlCache.get(crlUri);
+		SoftReference<X509CRL> crlRef = this.crlCache.get(crlUri);
+		if (null == crlRef) {
+			LOG.debug("no CRL entry found: " + crlUri);
+			return refreshCrl(crlUri, validationDate);
+		}
+		X509CRL crl = crlRef.get();
 		if (null == crl) {
+			LOG.debug("CRL garbage collected: " + crlUri);
 			return refreshCrl(crlUri, validationDate);
 		}
 		if (validationDate.after(crl.getNextUpdate())) {
+			LOG.debug("CRL no longer valid: " + crlUri);
+			LOG.debug("validation date: " + validationDate);
+			LOG.debug("CRL next update: " + crl.getNextUpdate());
+			return refreshCrl(crlUri, validationDate);
+		}
+		/*
+		 * The the Belgian PKI the nextUpdate CRL extension indicates 7 days.
+		 * The actual CRL refresh rate is every 3 hours. So it's a bit dangerous
+		 * to only base the CRL cache refresh strategy on the nextUpdate field
+		 * as indicated by the CRL.
+		 */
+		Date thisUpdate = crl.getThisUpdate();
+		DateTime cacheMaturityDateTime = new DateTime(thisUpdate)
+				.plusHours(this.cacheAgingHours);
+		if (validationDate.after(cacheMaturityDateTime.toDate())) {
+			LOG.debug("refreshing the CRL cache: " + crlUri);
 			return refreshCrl(crlUri, validationDate);
 		}
 		LOG.debug("using cached CRL: " + crlUri);
@@ -67,7 +96,25 @@ public class CachedCrlRepository implements CrlRepository {
 
 	private X509CRL refreshCrl(URI crlUri, Date validationDate) {
 		X509CRL crl = this.crlRepository.findCrl(crlUri, validationDate);
-		this.crlCache.put(crlUri, crl);
+		this.crlCache.put(crlUri, new SoftReference<X509CRL>(crl));
 		return crl;
+	}
+
+	/**
+	 * Gives back the CRL cache aging period in hours.
+	 * 
+	 * @return
+	 */
+	public int getCacheAgingHours() {
+		return this.cacheAgingHours;
+	}
+
+	/**
+	 * Sets the CRL cache aging period in hours.
+	 * 
+	 * @param cacheAgingHours
+	 */
+	public void setCacheAgingHours(int cacheAgingHours) {
+		this.cacheAgingHours = cacheAgingHours;
 	}
 }
