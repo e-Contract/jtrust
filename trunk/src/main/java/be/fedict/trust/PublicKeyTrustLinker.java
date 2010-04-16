50bad5d7e64ee465bf24c3f70a0a9c9aa17e3cb5
wim.vandenhaute <wim.vandenhaute@2407b8e0-faca-11dd-a8e2-f7cb3c35fcff>
2010-04-16 10:01:32 +0000

    AKID/SKID sanity checks

--- trunk/src/main/java/be/fedict/trust/PublicKeyTrustLinker.java
+++ trunk/src/main/java/be/fedict/trust/PublicKeyTrustLinker.java
@@ -18,11 +18,20 @@
 
 package be.fedict.trust;
 
+import java.io.IOException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.X509Extensions;
+import org.bouncycastle.x509.extension.AuthorityKeyIdentifierStructure;
+import org.bouncycastle.x509.extension.SubjectKeyIdentifierStructure;
+import org.bouncycastle.x509.extension.X509ExtensionUtil;
 
 /**
  * Public key trust linker implementation. Performs simple sanity checks based
@@ -106,10 +115,105 @@ public class PublicKeyTrustLinker implements TrustLinker {
 					TrustLinkerResultReason.INVALID_TRUST,
 					"child should not be a CA");
 		}
+
+		/*
+		 * SKID/AKID sanity check
+		 */
+		boolean isCa = isCa(certificate);
+		boolean isChildCa = isCa(childCertificate);
+
+		byte[] subjectKeyIdentifierData = certificate
+				.getExtensionValue(X509Extensions.SubjectKeyIdentifier.getId());
+		byte[] authorityKeyIdentifierData = childCertificate
+				.getExtensionValue(X509Extensions.AuthorityKeyIdentifier
+						.getId());
+
+		if (isCa && null == subjectKeyIdentifierData) {
+			LOG
+					.debug("certificate is CA and MUST contain a Subject Key Identifier");
+			return new TrustLinkerResult(false,
+					TrustLinkerResultReason.INVALID_TRUST,
+					"certificate is CA and  MUST contain a Subject Key Identifier");
+		}
+
+		if (isChildCa && null == authorityKeyIdentifierData) {
+			LOG
+					.debug("child certificate is CA and MUST contain an Authority Key Identifier");
+			return new TrustLinkerResult(false,
+					TrustLinkerResultReason.INVALID_TRUST,
+					"child certificate is CA and MUST contain an Authority Key Identifier");
+		}
+
+		if (null != subjectKeyIdentifierData
+				&& null != authorityKeyIdentifierData) {
+
+			AuthorityKeyIdentifierStructure authorityKeyIdentifierStructure;
+			try {
+				authorityKeyIdentifierStructure = new AuthorityKeyIdentifierStructure(
+						authorityKeyIdentifierData);
+			} catch (IOException e) {
+				LOG.debug("Error parsing authority key identifier structure");
+				return new TrustLinkerResult(false,
+						TrustLinkerResultReason.INVALID_TRUST,
+						"Error parsing authority key identifier structure");
+			}
+			String akidId = new String(Hex
+					.encodeHex(authorityKeyIdentifierStructure
+							.getKeyIdentifier()));
+
+			SubjectKeyIdentifierStructure subjectKeyIdentifierStructure;
+			try {
+				subjectKeyIdentifierStructure = new SubjectKeyIdentifierStructure(
+						subjectKeyIdentifierData);
+			} catch (IOException e) {
+				LOG.debug("Error parsing subject key identifier structure");
+				return new TrustLinkerResult(false,
+						TrustLinkerResultReason.INVALID_TRUST,
+						"Error parsing subject key identifier structure");
+			}
+			String skidId = new String(
+					Hex.encodeHex(subjectKeyIdentifierStructure
+							.getKeyIdentifier()));
+
+			if (!skidId.equals(akidId)) {
+				LOG
+						.debug("certificate's subject key identifier does not match child certificate's authority key identifier");
+				return new TrustLinkerResult(
+						false,
+						TrustLinkerResultReason.INVALID_TRUST,
+						"certificate's subject key identifier does not match child certificate's authority key identifier");
+			}
+		}
+
 		/*
 		 * We don't check pathLenConstraint since this one is only there to
 		 * protect the PKI business.
 		 */
 		return null;
+	}
+
+	private boolean isCa(X509Certificate certificate) {
+
+		byte[] basicConstraintsValue = certificate
+				.getExtensionValue(X509Extensions.BasicConstraints.getId());
+		if (null == basicConstraintsValue)
+			return false;
+
+		ASN1Encodable basicConstraintsDecoded;
+		try {
+			basicConstraintsDecoded = X509ExtensionUtil
+					.fromExtensionValue(basicConstraintsValue);
+		} catch (IOException e) {
+			LOG.error("IO error", e);
+			return false;
+		}
+		if (false == basicConstraintsDecoded instanceof ASN1Sequence) {
+			LOG.debug("basic constraints extension is not an ASN1 sequence");
+			return false;
+		}
+		ASN1Sequence basicConstraintsSequence = (ASN1Sequence) basicConstraintsDecoded;
+		BasicConstraints basicConstraints = new BasicConstraints(
+				basicConstraintsSequence);
+		return basicConstraints.isCA();
 	}
 }
