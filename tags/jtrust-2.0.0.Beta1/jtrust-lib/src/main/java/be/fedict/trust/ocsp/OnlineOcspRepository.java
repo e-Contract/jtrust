4506568e42ff0bfda2cdbd692e239a313adb1870
frank.cornelis <frank.cornelis@2407b8e0-faca-11dd-a8e2-f7cb3c35fcff>
2013-10-18 08:03:09 +0000

    [maven-release-plugin]  copy for tag jtrust-2.0.0.Beta1

---
+++ tags/jtrust-2.0.0.Beta1/jtrust-lib/src/main/java/be/fedict/trust/ocsp/OnlineOcspRepository.java
@@ -0,0 +1,181 @@
+/*
+ * Java Trust Project.
+ * Copyright (C) 2009 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.trust.ocsp;
+
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.security.cert.X509Certificate;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.bouncycastle.cert.ocsp.CertificateID;
+import org.bouncycastle.cert.ocsp.OCSPReq;
+import org.bouncycastle.cert.ocsp.OCSPReqBuilder;
+import org.bouncycastle.cert.ocsp.OCSPResp;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.DigestCalculatorProvider;
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+
+import be.fedict.trust.Credentials;
+import be.fedict.trust.NetworkConfig;
+
+/**
+ * Online OCSP repository. This implementation will contact the OCSP Responder
+ * to retrieve the OCSP response.
+ * 
+ * @author Frank Cornelis
+ * 
+ */
+public class OnlineOcspRepository implements OcspRepository {
+
+	private static final Log LOG = LogFactory
+			.getLog(OnlineOcspRepository.class);
+
+	private final NetworkConfig networkConfig;
+
+	private Credentials credentials;
+
+	/**
+	 * Main construtor.
+	 * 
+	 * @param networkConfig
+	 *            the optional network configuration used during OCSP Responder
+	 *            communication.
+	 */
+	public OnlineOcspRepository(NetworkConfig networkConfig) {
+		this.networkConfig = networkConfig;
+	}
+
+	/**
+	 * Default constructor.
+	 */
+	public OnlineOcspRepository() {
+		this(null);
+	}
+
+	/**
+	 * Sets the credentials to use to access protected OCSP services.
+	 * 
+	 * @param credentials
+	 */
+	public void setCredentials(Credentials credentials) {
+		this.credentials = credentials;
+	}
+
+	public OCSPResp findOcspResponse(URI ocspUri, X509Certificate certificate,
+			X509Certificate issuerCertificate) {
+		OCSPResp ocspResp = null;
+		try {
+			ocspResp = getOcspResponse(ocspUri, certificate, issuerCertificate);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return ocspResp;
+	}
+
+	private OCSPResp getOcspResponse(URI ocspUri, X509Certificate certificate,
+			X509Certificate issuerCertificate) throws Exception {
+		LOG.debug("OCSP URI: " + ocspUri);
+		OCSPReqBuilder ocspReqBuilder = new OCSPReqBuilder();
+		DigestCalculatorProvider digCalcProv = new JcaDigestCalculatorProviderBuilder()
+				.setProvider(BouncyCastleProvider.PROVIDER_NAME).build();
+		CertificateID certId = new CertificateID(
+				digCalcProv.get(CertificateID.HASH_SHA1),
+				new JcaX509CertificateHolder(issuerCertificate),
+				certificate.getSerialNumber());
+		ocspReqBuilder.addRequest(certId);
+
+		OCSPReq ocspReq = ocspReqBuilder.build();
+		byte[] ocspReqData = ocspReq.getEncoded();
+
+		HttpPost httpPost = new HttpPost(ocspUri.toString());
+		ContentType contentType = ContentType
+				.create("application/ocsp-request");
+		HttpEntity requestEntity = new ByteArrayEntity(ocspReqData, contentType);
+		httpPost.addHeader("User-Agent", "jTrust OCSP Client");
+		httpPost.setEntity(requestEntity);
+
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		if (null != this.networkConfig) {
+			HttpHost proxy = new HttpHost(this.networkConfig.getProxyHost(),
+					this.networkConfig.getProxyPort());
+			httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
+					proxy);
+		}
+		if (null != this.credentials) {
+			this.credentials.init(httpClient.getCredentialsProvider());
+		}
+
+		HttpResponse httpResponse;
+		int responseCode;
+		try {
+			httpResponse = httpClient.execute(httpPost);
+			StatusLine statusLine = httpResponse.getStatusLine();
+			responseCode = statusLine.getStatusCode();
+		} catch (ConnectException e) {
+			LOG.debug("OCSP responder is down");
+			return null;
+		}
+
+		if (HttpURLConnection.HTTP_OK != responseCode) {
+			LOG.error("HTTP response code: " + responseCode);
+			return null;
+		}
+
+		Header responseContentTypeHeader = httpResponse
+				.getFirstHeader("Content-Type");
+		if (null == responseContentTypeHeader) {
+			LOG.debug("no Content-Type response header");
+			return null;
+		}
+		String resultContentType = responseContentTypeHeader.getValue();
+		if (!"application/ocsp-response".equals(resultContentType)) {
+			LOG.debug("result content type not application/ocsp-response");
+			return null;
+		}
+
+		Header responseContentLengthHeader = httpResponse
+				.getFirstHeader("Content-Length");
+		if (null != responseContentLengthHeader) {
+			String resultContentLength = responseContentLengthHeader.getValue();
+			if ("0".equals(resultContentLength)) {
+				LOG.debug("no content returned");
+				return null;
+			}
+		}
+
+		HttpEntity httpEntity = httpResponse.getEntity();
+		OCSPResp ocspResp = new OCSPResp(httpEntity.getContent());
+		LOG.debug("OCSP response size: " + ocspResp.getEncoded().length
+				+ " bytes");
+		return ocspResp;
+	}
+}
---
+++ tags/jtrust-2.0.0.Beta1/jtrust-lib/src/test/java/test/unit/be/fedict/trust/constraints/EndEntityCertificateConstraintTest.java
@@ -0,0 +1,128 @@
+/*
+ * Java Trust Project.
+ * Copyright (C) 2009 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package test.unit.be.fedict.trust.constraints;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.security.KeyPair;
+import java.security.cert.X509Certificate;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import test.unit.be.fedict.trust.TrustTestUtils;
+import be.fedict.trust.constraints.EndEntityCertificateConstraint;
+import be.fedict.trust.linker.TrustLinkerResultException;
+import be.fedict.trust.linker.TrustLinkerResultReason;
+
+public class EndEntityCertificateConstraintTest {
+
+	private EndEntityCertificateConstraint testedInstance;
+
+	@Before
+	public void setUp() throws Exception {
+		this.testedInstance = new EndEntityCertificateConstraint();
+	}
+
+	@Test
+	public void testEmptyEndEntities() throws Exception {
+		// setup
+		DateTime notBefore = new DateTime();
+		DateTime notAfter = notBefore.plusMonths(1);
+
+		KeyPair rootKeyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate rootCertificate = TrustTestUtils
+				.generateSelfSignedCertificate(rootKeyPair, "CN=TestRoot",
+						notBefore, notAfter);
+
+		KeyPair keyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate certificate = TrustTestUtils
+				.generateCertificate(keyPair.getPublic(), "CN=Test", notBefore,
+						notAfter, rootCertificate, rootKeyPair.getPrivate(),
+						false, 0, null, null);
+
+		// operate
+		try {
+			this.testedInstance.check(certificate);
+			fail();
+		} catch (TrustLinkerResultException e) {
+			assertEquals(TrustLinkerResultReason.CONSTRAINT_VIOLATION,
+					e.getReason());
+		}
+	}
+
+	@Test
+	public void testEndEntityMisMatch() throws Exception {
+		// setup
+		DateTime notBefore = new DateTime();
+		DateTime notAfter = notBefore.plusMonths(1);
+
+		KeyPair rootKeyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate rootCertificate = TrustTestUtils
+				.generateSelfSignedCertificate(rootKeyPair, "CN=TestRoot",
+						notBefore, notAfter);
+
+		KeyPair keyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate certificate = TrustTestUtils
+				.generateCertificate(keyPair.getPublic(), "CN=Test", notBefore,
+						notAfter, rootCertificate, rootKeyPair.getPrivate(),
+						false, 0, null, null);
+
+		KeyPair endKeyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate endCertificate = TrustTestUtils.generateCertificate(
+				endKeyPair.getPublic(), "CN=TestEnd", notBefore, notAfter,
+				certificate, keyPair.getPrivate(), false, 0, null, null);
+
+		this.testedInstance.addEndEntity(endCertificate);
+
+		// operate
+		try {
+			this.testedInstance.check(certificate);
+			fail();
+		} catch (TrustLinkerResultException e) {
+			assertEquals(TrustLinkerResultReason.CONSTRAINT_VIOLATION,
+					e.getReason());
+		}
+	}
+
+	@Test
+	public void testEndEntityMatch() throws Exception {
+		// setup
+		DateTime notBefore = new DateTime();
+		DateTime notAfter = notBefore.plusMonths(1);
+
+		KeyPair rootKeyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate rootCertificate = TrustTestUtils
+				.generateSelfSignedCertificate(rootKeyPair, "CN=TestRoot",
+						notBefore, notAfter);
+
+		KeyPair keyPair = TrustTestUtils.generateKeyPair();
+		X509Certificate certificate = TrustTestUtils
+				.generateCertificate(keyPair.getPublic(), "CN=Test", notBefore,
+						notAfter, rootCertificate, rootKeyPair.getPrivate(),
+						false, 0, null, null);
+
+		this.testedInstance.addEndEntity(certificate);
+
+		// operate
+		this.testedInstance.check(certificate);
+	}
+}
