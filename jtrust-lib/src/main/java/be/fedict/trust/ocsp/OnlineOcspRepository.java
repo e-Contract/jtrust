6f29737092ae6488aad1123175c7d3f9af641a9f
Frank Cornelis <info@e-contract.be>
2018-10-15 08:42:39 +0200

    support for OCSP nonce

--- jtrust-lib/src/main/java/be/fedict/trust/ocsp/OnlineOcspRepository.java
+++ jtrust-lib/src/main/java/be/fedict/trust/ocsp/OnlineOcspRepository.java
@@ -22,6 +22,7 @@ package be.fedict.trust.ocsp;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
@@ -37,7 +38,16 @@ import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.ocsp.OCSPObjectIdentifiers;
+import org.bouncycastle.asn1.ocsp.OCSPResponseStatus;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.Extensions;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.bouncycastle.cert.ocsp.BasicOCSPResp;
 import org.bouncycastle.cert.ocsp.CertificateID;
 import org.bouncycastle.cert.ocsp.OCSPReq;
 import org.bouncycastle.cert.ocsp.OCSPReqBuilder;
@@ -45,10 +55,10 @@ import org.bouncycastle.cert.ocsp.OCSPResp;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.DigestCalculatorProvider;
 import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+import org.bouncycastle.util.Arrays;
 
 import be.fedict.trust.Credentials;
 import be.fedict.trust.NetworkConfig;
-import org.apache.http.util.EntityUtils;
 
 /**
  * Online OCSP repository. This implementation will contact the OCSP Responder
@@ -59,8 +69,7 @@ import org.apache.http.util.EntityUtils;
  */
 public class OnlineOcspRepository implements OcspRepository {
 
-	private static final Log LOG = LogFactory
-			.getLog(OnlineOcspRepository.class);
+	private static final Log LOG = LogFactory.getLog(OnlineOcspRepository.class);
 
 	private final NetworkConfig networkConfig;
 
@@ -94,8 +103,8 @@ public class OnlineOcspRepository implements OcspRepository {
 	}
 
 	@Override
-	public OCSPResp findOcspResponse(URI ocspUri, X509Certificate certificate,
-			X509Certificate issuerCertificate, Date validationDate) {
+	public OCSPResp findOcspResponse(URI ocspUri, X509Certificate certificate, X509Certificate issuerCertificate,
+			Date validationDate) {
 		if (null == ocspUri) {
 			return null;
 		}
@@ -108,34 +117,37 @@ public class OnlineOcspRepository implements OcspRepository {
 		return ocspResp;
 	}
 
-	private OCSPResp getOcspResponse(URI ocspUri, X509Certificate certificate,
-			X509Certificate issuerCertificate) throws Exception {
+	private OCSPResp getOcspResponse(URI ocspUri, X509Certificate certificate, X509Certificate issuerCertificate)
+			throws Exception {
 		LOG.debug("OCSP URI: " + ocspUri);
 		OCSPReqBuilder ocspReqBuilder = new OCSPReqBuilder();
 		DigestCalculatorProvider digCalcProv = new JcaDigestCalculatorProviderBuilder()
 				.setProvider(BouncyCastleProvider.PROVIDER_NAME).build();
-		CertificateID certId = new CertificateID(
-				digCalcProv.get(CertificateID.HASH_SHA1),
-				new JcaX509CertificateHolder(issuerCertificate),
-				certificate.getSerialNumber());
+		CertificateID certId = new CertificateID(digCalcProv.get(CertificateID.HASH_SHA1),
+				new JcaX509CertificateHolder(issuerCertificate), certificate.getSerialNumber());
 		ocspReqBuilder.addRequest(certId);
+
+		byte[] nonce = new byte[20];
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.nextBytes(nonce);
+		DEROctetString encodedNonceValue = new DEROctetString(new DEROctetString(nonce).getEncoded());
+		Extension extension = new Extension(OCSPObjectIdentifiers.id_pkix_ocsp_nonce, false, encodedNonceValue);
+		Extensions extensions = new Extensions(extension);
+		ocspReqBuilder.setRequestExtensions(extensions);
 
 		OCSPReq ocspReq = ocspReqBuilder.build();
 		byte[] ocspReqData = ocspReq.getEncoded();
 
 		HttpPost httpPost = new HttpPost(ocspUri.toString());
-		ContentType contentType = ContentType
-				.create("application/ocsp-request");
+		ContentType contentType = ContentType.create("application/ocsp-request");
 		HttpEntity requestEntity = new ByteArrayEntity(ocspReqData, contentType);
 		httpPost.addHeader("User-Agent", "jTrust OCSP Client");
 		httpPost.setEntity(requestEntity);
 
 		DefaultHttpClient httpClient = new DefaultHttpClient();
 		if (null != this.networkConfig) {
-			HttpHost proxy = new HttpHost(this.networkConfig.getProxyHost(),
-					this.networkConfig.getProxyPort());
-			httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
-					proxy);
+			HttpHost proxy = new HttpHost(this.networkConfig.getProxyHost(), this.networkConfig.getProxyPort());
+			httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
 		}
 		if (null != this.credentials) {
 			this.credentials.init(httpClient.getCredentialsProvider());
@@ -157,8 +169,7 @@ public class OnlineOcspRepository implements OcspRepository {
 			return null;
 		}
 
-		Header responseContentTypeHeader = httpResponse
-				.getFirstHeader("Content-Type");
+		Header responseContentTypeHeader = httpResponse.getFirstHeader("Content-Type");
 		if (null == responseContentTypeHeader) {
 			LOG.error("no Content-Type response header");
 			return null;
@@ -166,15 +177,14 @@ public class OnlineOcspRepository implements OcspRepository {
 		String resultContentType = responseContentTypeHeader.getValue();
 		if (!"application/ocsp-response".equals(resultContentType)) {
 			LOG.error("result content type not application/ocsp-response");
-                        LOG.error("actual content-type: " + resultContentType);
-                        if ("text/html".equals(resultContentType)) {
-                            LOG.error("content: " + EntityUtils.toString(httpResponse.getEntity()));
-                        }
+			LOG.error("actual content-type: " + resultContentType);
+			if ("text/html".equals(resultContentType)) {
+				LOG.error("content: " + EntityUtils.toString(httpResponse.getEntity()));
+			}
 			return null;
 		}
 
-		Header responseContentLengthHeader = httpResponse
-				.getFirstHeader("Content-Length");
+		Header responseContentLengthHeader = httpResponse.getFirstHeader("Content-Length");
 		if (null != responseContentLengthHeader) {
 			String resultContentLength = responseContentLengthHeader.getValue();
 			if ("0".equals(resultContentLength)) {
@@ -185,9 +195,32 @@ public class OnlineOcspRepository implements OcspRepository {
 
 		HttpEntity httpEntity = httpResponse.getEntity();
 		OCSPResp ocspResp = new OCSPResp(httpEntity.getContent());
-		LOG.debug("OCSP response size: " + ocspResp.getEncoded().length
-				+ " bytes");
+		LOG.debug("OCSP response size: " + ocspResp.getEncoded().length + " bytes");
 		httpPost.releaseConnection();
+
+		int ocspRespStatus = ocspResp.getStatus();
+		if (OCSPResponseStatus.SUCCESSFUL != ocspRespStatus) {
+			LOG.debug("OCSP response status: " + ocspRespStatus);
+			return ocspResp;
+		}
+
+		Object responseObject = ocspResp.getResponseObject();
+		BasicOCSPResp basicOCSPResp = (BasicOCSPResp) responseObject;
+		Extension nonceExtension = basicOCSPResp.getExtension(OCSPObjectIdentifiers.id_pkix_ocsp_nonce);
+		if (null == nonceExtension) {
+			LOG.debug("no nonce extension is response");
+			return ocspResp;
+		}
+
+		ASN1OctetString nonceExtensionValue = extension.getExtnValue();
+		ASN1Primitive nonceValue = ASN1Primitive.fromByteArray(nonceExtensionValue.getOctets());
+		byte[] responseNonce = ((DEROctetString) nonceValue).getOctets();
+		if (!Arrays.areEqual(nonce, responseNonce)) {
+			LOG.error("nonce mismatch");
+			return null;
+		}
+		LOG.debug("nonce match");
+
 		return ocspResp;
 	}
 }
